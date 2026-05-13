eeeb0681b9c704e9e07785eddab48e54753d681c
joemakepeace7 <joemakepeace7@gmail.com>
2026-05-13 09:33:47 +0100

    Exercise 1 & 2 Complete

--- Exercise 1 - Storing User Data/Program.cs
+++ Exercise 1 - Storing User Data/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int x = 2;
+int y = 790;
+
+int remainder = y % x;
+
+if (remainder == 0)
+{
+    Console.WriteLine("The number is even.");
+}
+else
+{
+    Console.WriteLine("The number is odd.");
+}
