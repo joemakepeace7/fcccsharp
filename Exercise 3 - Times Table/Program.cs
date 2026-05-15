3f18e485a544776c26bfee91d11ad9ce6805276d
joemakepeace7 <joemakepeace7@gmail.com>
2026-05-15 10:52:17 +0100

    Exercise 3 Complete

--- Exercise 3 - Times Table/Program.cs
+++ Exercise 3 - Times Table/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿/*
+ * Ask user for a number for the times table, then print the times table for that number up to the given number
+ */
+
+Console.Write("Enter a number for the times table: ");
+string numInput = Console.ReadLine();
+
+/*
+ * Take numInput and try parsing as an integer. If that works, then output that integer into the integer 'result', which is initialised directly in the if statement
+ * If anything other than an integer is entered, the else statement will execute.
+ */
+if (int.TryParse(numInput, out int result))
+{
+    for (int i = 1; i <= result; i++)
+    {
+        Console.WriteLine($"{i * result} Tigers");
+        /*
+         * $" is required for string interpolation, allowing for inserting variables into strings.
+         */
+    };
+
+}
+else
+{
+    Console.WriteLine("Enter a valid number");
+}
+
+
+Console.ReadLine();
+
